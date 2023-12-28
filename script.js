d0248bfb4803ba4e5aa29ce58ececeb9f5f036b9
ajith <142565103+Ajith31kumar@users.noreply.github.com>
2023-12-28 16:46:21 +0530

    first commit

--- script.js
+++ script.js
@@ -35,17 +35,3 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycby8K_3fzZDDDlLzLaAv83
       })
       .catch(error => console.error('Error!', error.message))
   })
-
-//   #projects{
-//     margin-top: 850px  !important;
-// }
-// /.view{
-//     background-color: #28B463;
-//     color: #fff;
-//     text-decoration: none !important;
-//     padding: 20px 5px 20px 15px;
-//     width: 200px;
-//     margin: 50px 0px 0px 60px;
-//     border-radius: 10px;
-    
-// } 
